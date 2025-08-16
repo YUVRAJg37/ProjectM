313bef0f14f2d3d0126fd183b9209c8b6ef4d23e
Yuvi <yuvraj.yash2001@gmail.com>
2025-08-17 02:53:58 +0530

    Update | Formatting

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,8 +1,8 @@
-import { WebGLRenderer } from "three"
+import { WebGLRenderer } from "three";
 
-export function createRenderer(container: HTMLElement){
-    const renderer = new WebGLRenderer();
-    renderer.setSize(container.clientWidth, container.clientHeight)
-    container.appendChild(renderer.domElement)
-    return renderer
+export function createRenderer(container: HTMLElement) {
+  const renderer = new WebGLRenderer();
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  container.appendChild(renderer.domElement);
+  return renderer;
 }
